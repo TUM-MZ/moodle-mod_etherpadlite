53e1b8e5c0851c49f7a91fea2488e61a23e13714
Dmytro Vorona <alendit@gmail.com>
2014-11-05 14:32:10 +0100

    Replaced umlauts with escapes

--- lang/de/etherpadlite.php
+++ lang/de/etherpadlite.php
@@ -7,20 +7,20 @@
  * @subpackage etherpadlite
  *
  * @author     Timo Welde <tjwelde@gmail.com>
- * @copyright  2012 Humboldt-Universität zu Berlin <moodle-support@cms.hu-berlin.de>
+ * @copyright  2012 Humboldt-Universit&aumlt zu Berlin <moodle-support@cms.hu-berlin.de>
  * @license    http://www.gnu.org/copyleft/gpl.html GNU GPL v3 or later
  */
 
 $string['etherpadlite'] = 'Etherpad Lite';
-$string['modulename_help'] = 'Das Etherpad Lite modul erlaubt es Studenten und Lehrenden kollaborativ zu schreiben (Der Text wird synchronisiert, während sie schreiben)
+$string['modulename_help'] = 'Das Etherpad Lite modul erlaubt es Studenten und Lehrenden kollaborativ zu schreiben (Der Text wird synchronisiert, w&aumlhrend sie schreiben)
 
-(Der Etherpad-Server, welcher hinter diesem Modul steht, ist noch im beta Stadium. Deshalb können Probleme auftreten, was allerdings nicht erwartet wird)';
+(Der Etherpad-Server, welcher hinter diesem Modul steht, ist noch im beta Stadium. Deshalb k&oumlnnen Probleme auftreten, was allerdings nicht erwartet wird)';
 
 $string['etherpadlitename'] = 'Pad-Name';
 
-$string['etherpadlite:addinstance'] = 'Neues Pad hinzufügen';
-$string['guestsallowed'] = 'Dürfen Gäste das Pad ändern?';
-$string['guestsallowed_help'] = 'Diese Einstellung bestimmt, ob die Gäste das Pad beschreiben können. Ansonsten ist ihnen nur ein Lesezugriff gewährt.';
+$string['etherpadlite:addinstance'] = 'Neues Pad hinzuf&uumlgen';
+$string['guestsallowed'] = 'D&uumlrfen G&aumlste das Pad &aumlndern?';
+$string['guestsallowed_help'] = 'Diese Einstellung bestimmt, ob die G&aumlste das Pad beschreiben k&oumlnnen. Ansonsten ist ihnen nur ein Lesezugriff gew&aumlhrt.';
 
 // view
 $string['summaryguest'] = 'Sie sind als Gast eingeloggt, deshalb sehen Sie nur den reinen Text dieses Pads. Um neue &Auml;nderungen zu sehen, laden Sie diese Seite neu.';
